496049c3ee53ab26062f69ff69e0e6fbeaf597c3
willisplummer <willisplummer@gmail.com>
2017-05-27 13:59:29 -0400

    hyper step 1

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,17 @@
 module Main where
 
 import Prelude
+import Control.IxMonad ((:*>))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE)
+import Hyper.Node.Server (defaultOptionsWithLogging, runServer)
+import Hyper.Response (closeHeaders, respond, writeStatus)
+import Hyper.Status (statusOK)
+import Node.HTTP (HTTP)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+main :: forall e. Eff (console :: CONSOLE, http :: HTTP | e) Unit
+main =
+  let app = writeStatus statusOK
+            :*> closeHeaders
+            :*> respond "Hello, Hyper!"
+  in runServer defaultOptionsWithLogging {} app
